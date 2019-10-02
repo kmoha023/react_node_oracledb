c3a2aa64b66e048edb403b7ccb84cf574fbeaa02
P2884288 <C-Khaja.M.Mohammed@charter.com>
2019-10-02 14:04:04 -0500

    Initial commit

---
+++ services/web-server.js
@@ -0,0 +1,62 @@
+const http = require('http');
+const express = require('express');
+const webServerConfig = require('../shared/web-server.js');
+const morgan = require('morgan');
+const database = require('./database.js');
+
+let httpServer;
+
+function initialize() {
+    return new Promise((resolve, reject) => {
+        const app = express();
+        httpServer = http.createServer(app);
+
+        /*Morgan middleware for logging */
+        app.use(morgan('combined'));
+
+        // get db query
+        app.get('/', async (req, res) => {
+            const result = await database.simpleExecute('select * from DBA_REPORTS.MV_PRD_APEX_SUMMARY_REP');
+                console.log("Result from DB...", result.rows);
+            // const DUMMY = result.rows[0].DATABASE_NAME;
+            // const DUMMY = result.rows[0].APPLICATION_NAME;
+            // const DUMMY = result.rows[0].ENVIRONMENT;
+            // const DUMMY = result.rows[0].HOST_NAME;
+            // const DUMMY = result.rows[0].DATABASE_ROLE;
+            // const DUMMY = result.rows[0].DC_LOCATION;
+            // const date = result.rows[0].SYSTIMESTAMP;
+
+            res.end(`DB user: ${DUMMY}\n`);
+        });
+
+        app.get('/', (req, res) => {
+            res.end('Hello World!');
+        });
+
+        httpServer.listen(webServerConfig.port)
+            .on('listening', () => {
+                console.log(`Web server listening on localhost:${webServerConfig.port}`);
+
+                resolve();
+            })
+            .on('error', err => {
+                reject(err);
+            });
+    });
+}
+
+function close() {
+    return new Promise((resolve, reject) => {
+        httpServer.close((err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve();
+        });
+    });
+}
+
+module.exports.close = close;
+module.exports.initialize = initialize;
---
+++ shared/database.js
@@ -0,0 +1,21 @@
+module.exports = {
+    hrPool: {
+        user: "P2884288",
+        password: "Khaja1234",
+        connectString: "vm0dnoravsd0085.twcable.com:1521/spapx01d",
+        poolMin: 10,
+        poolMax: 10,
+        poolIncrement: 0
+    }
+};
+
+// module.exports = {
+//     hrPool: {
+//         user: process.env.HR_USER || "spapx01d",
+//         password: process.env.HR_PASSWORD || "Khaja01234",
+//         connectString: process.env.HR_CONNECTIONSTRING || "vm0dnoravsd0085.twcable.com",
+//         poolMin: 10,
+//         poolMax: 10,
+//         poolIncrement: 0
+//     }
+// };
